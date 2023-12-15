b18ee430383bb8433bafa7e6f497c94bf3251fa9
JayvonK <jaykhuth@gmail.com>
2023-12-14 23:16:42 -0800

    js

--- scripts/app.js
+++ scripts/app.js
@@ -3,3 +3,43 @@ let studentEmail = document.getElementById("studentEmail");
 let studentAge = document.getElementById("studentAge");
 let studentNumber = document.getElementById("studentNumber");
 let errorMessage = document.getElementById("errorMessage");
+let userInput = document.getElementById("userInput");
+let searchBtn = document.getElementById("searchBtn");
+
+let studentList;
+let matchingStudent;
+
+async function StudentListCall(){
+    const promise = await fetch('../data/studentList.json');
+    const data = promise.json();
+
+    studentList = data.studentList;
+}
+
+searchBtn.addEventListener('click', function(e){
+    FindStudent();
+})
+
+function FindStudent(){
+    let input = userInput.value.toLowerCase();
+
+    console.log(studentList.length)
+
+    for(let i = 0; i < studentList.length; i++){
+        let currentStudent = studentList[i];
+
+        if(input === currentStudent.firstName.toLowerCase()){
+            matchingStudent = currentStudent;
+        }
+    }
+
+    if(matchingStudent){
+        studentName.textContent = matchingStudent.firstName + " " + matchingStudent.lastName;
+        studentEmail.textContent = matchingStudent.email;
+
+    }else{
+        errorMessage.textContent = "Student Not Found";
+    }
+}
+
+StudentListCall();
