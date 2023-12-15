029bebd78a8e887af8ca297d5daacb2e0d9ce095
JayvonK <jaykhuth@gmail.com>
2023-12-14 22:27:55 -0800

    setting up

---
+++ scripts/app.js
@@ -0,0 +1,5 @@
+let studentName = document.getElementById("studentName");
+let studentEmail = document.getElementById("studentEmail");
+let studentAge = document.getElementById("studentAge");
+let studentNumber = document.getElementById("studentNumber");
+let errorMessage = document.getElementById("errorMessage");
